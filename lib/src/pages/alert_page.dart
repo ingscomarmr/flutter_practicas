cba2b4ed6c51a5ab34541f7000882c67ddbe125e
Omar Munguia <ingscomarmr@gmail.com>
2020-05-22 22:11:59 -0500

    Alerta simple con cierre al clic fuera

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -13,6 +13,31 @@ class AlertPage extends StatelessWidget {
           Navigator.pop(context); //para regresar a la pesataña anterior
         }
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed:() => _mostrarAlerta(context), //para mandar un parametro a un metodo         
+        ),
+      ),
     );
   }
+
+
+  void _mostrarAlerta(BuildContext context){
+    showDialog(
+      context: context,
+      barrierDismissible: true,//si haces clic fuera de la alerta se cierra
+      builder: (context){
+        return AlertDialog( //simple alerta
+          title: Text('Titulo'),
+          content: Text('Contenido...'),
+        );
+      }
+    );
+  }
+
+
 }
