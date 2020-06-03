9b4324187fcf91715142ac4c96ddbc7f7dda4c41
Omar Munguia <ingscomarmr@gmail.com>
2020-06-02 19:27:27 -0500

    items dinamicos listview builder ScrollController

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -8,7 +8,29 @@ class ListaPage extends StatefulWidget {
 }
 
 class _ListaPageState extends State<ListaPage> {
-  List<int> _numeros = [1,2,3,4,5,6,7];
+
+  ScrollController _scrollController = ScrollController();
+
+  List<int> _indexImgList = List();
+  int _ultimoItem = 0;
+
+  @override
+  void initState() { //cuando inicia la etapa se ejecuta
+    super.initState(); //esto es necesario
+    _agregarImg();//para agregar los elementos a el index de imgs
+
+    //vamos a estar checando el scroll por medio del scrollcontroller en el listview builder
+    _scrollController.addListener(() {
+      //print('Scroll!!');//se ejecuta al mover el scroll
+      
+      if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent){ //cuando la posicion del scroll esta en el maximo del scroll
+        //si la posicion del scroll esta en el limite se ejecuta
+        _agregarImg(); //volvemos a ejecutar el metodos        
+      }
+
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -22,9 +44,10 @@ class _ListaPageState extends State<ListaPage> {
 
   Widget _crearList(){
     return ListView.builder( //se utiliza cuando los elementos de una lista a mostrar pueden ser infinitos o dinamicos
-      itemCount: _numeros.length, //el numero de elemento esperadados
+      controller: _scrollController, //para poder controlar el scroll
+      itemCount: _indexImgList.length, //el numero de elemento esperadados
       itemBuilder: (BuildContext context, int index){ //se encarga de redenderizar los elementos de la pagina
-        final imgs = _numeros[index];
+        final imgs = _indexImgList[index];
         return FadeInImage(          
             image: NetworkImage('https://picsum.photos/500/300/?image=$imgs'),
             placeholder: AssetImage('assets/loagin_img.gif'),
@@ -34,4 +57,15 @@ class _ListaPageState extends State<ListaPage> {
   }
 
   
+  void _agregarImg(){
+    for(var i = 0; i < 10; i++){
+      _ultimoItem++;
+      _indexImgList.add(_ultimoItem);
+    }
+
+    setState(() {
+      
+    });
+  }
+
 }
